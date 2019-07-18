6eabf2886d2e04d5d58a43bc34e24eb430c1e883
Daniel Bustamante <dbustamante69@gmail.com>
2019-07-17 22:08:32 -0500

    initial commit

---
+++ src/main/kotlin/co/upet/phone/validator/PhoneInteractionOperations.kt
@@ -0,0 +1,7 @@
+package co.upet.phone.validator
+
+
+/**
+ * @author Daniel Bustamante Ospina <dbustamante69@gmail.com> on 7/15/19
+ */
+fun createPhoneInteraction(cmd: String, id: String) = "324234"
